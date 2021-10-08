a2faa0fa566143b15524cc2235c0aeff41fae7c7
thalles <thalles.freitas@zup.com.br>
2021-10-08 18:39:07 -0300

    remove unused
    Signed-off-by: thalles <thalles.freitas@zup.com.br>

--- main.js
+++ main.js
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-require('loadenv')();
 const { checkLicense } = require("./license");
 const core = require('@actions/core')
 const chalk = require('chalk')
